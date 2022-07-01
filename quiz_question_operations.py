7d3cd2ba97caa2dc44efaa0689d9513ff060a0c0
smalldirtybird <smalldirtybird@gmail.com>
2022-07-01 14:37:48 +0300

    Dry code

--- quiz_question_operations.py
+++ quiz_question_operations.py
@@ -8,7 +8,6 @@ def convert_quiz_files_to_dict():
     folder = os.environ['QUIZ_FILES_PATH']
     question_files = os.listdir(folder)
     quiz_content = defaultdict(dict)
-    count = 0
     for question_filename in question_files:
         question_filepath = os.path.join(folder, question_filename)
         with open(question_filepath, 'r', encoding='KOI8-R') as question_file:
@@ -18,27 +17,29 @@ def convert_quiz_files_to_dict():
                 if ':\n' in text_string:
                     header, value = text_string.split(sep=':\n', maxsplit=1)
                     if 'Вопрос' in header:
-                        count += 1
-                        quiz_content[str(count)]['question'] = value
+                        question = value
                     elif 'Ответ' in header:
-                        quiz_content[str(count)]['answer'] = value
+                        quiz_content[question] = value
     return quiz_content
 
 
-def get_new_question(database_id, connection):
-    quiz_questions = convert_quiz_files_to_dict()
-    question_number = str(random.randint(1, len(quiz_questions)))
-    quiz_question = quiz_questions[question_number]['question']
-    connection.set(database_id, question_number)
-    return quiz_question
+def get_new_question(user_id, connection):
+    question, answer = random.choice(
+        list(convert_quiz_files_to_dict().items()))
+    connection.set(user_id, answer)
+    print(answer)
+    return question
 
 
-def get_correct_answer(database_id, connection):
-    quiz_questions = convert_quiz_files_to_dict()
-    question_number = connection.get(database_id).decode('UTF-8')
-    correct_answer = quiz_questions[question_number]['answer']
+def get_correct_answer(user_id, connection):
+    correct_answer = connection.get(user_id).decode('UTF-8')
     if '.' in correct_answer or '(' in correct_answer:
-        answer, explanation = re.split('\.| \(', correct_answer, maxsplit=1)
-        return answer
-    else:
-        return correct_answer
+        correct_answer, explanation = re.split(
+            '\.| \(', correct_answer, maxsplit=1)
+    return correct_answer
+
+
+if __name__ == '__main__':
+    from dotenv import load_dotenv
+    load_dotenv()
+    print(type(convert_quiz_files_to_dict().items()))
